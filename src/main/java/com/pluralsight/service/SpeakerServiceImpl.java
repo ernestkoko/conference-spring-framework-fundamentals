d4ea3ec08af613ad674ad03da9ba8491965f2e72
ernestkoko <ernesteferetin@yahoo.com>
2020-04-16 12:26:35 +0100

    Manually wired up the app

--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -1,4 +1,18 @@
 package com.pluralsight.service;
 
-public class SpeakerServiceImpl {
+import com.pluralsight.mdoel.Speaker;
+import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
+import com.pluralsight.repository.SpeakerRepository;
+
+import java.util.List;
+
+public class SpeakerServiceImpl implements SpeakerService {
+
+    private SpeakerRepository repository = new HibernateSpeakerRepositoryImpl();
+
+    @Override
+    public List<Speaker> findAll(){
+        return repository.findAll();
+
+    }
 }
